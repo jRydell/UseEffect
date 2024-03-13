e63d9bd3feb895afd04f3116c150b3e5951fe8fa
jRydell <johannilsrydell89@gmail.com>
2024-03-13 19:16:35 +0100

    start

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+import Person from './Person'
+
+function App() {
+
+
+  return (
+    <>
+      <Person />
+    </>
+  )
+}
+
+export default App
---
+++ src/Person.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+
+
+type SWPerson = {
+    name: string;
+    eye_color: string;
+    birth_year: string;
+};
+
+
+const Person = () => {
+    //const [name, setName] = useState("");
+    const [person, setPerson] = useState({} as SWPerson);
+
+
+    useEffect(() => {
+        const url = "https://swapi.py4e.com/api/people/1/";
+        const fetchLuke = async () => {
+            const result = await fetch(url);  // result kan heta response
+            const luke = await result.json(); //luke kan heta data
+            if (!ignore) {
+                setPerson({
+                    name: luke.name,
+                    eye_color: luke.eye_color,
+                    birth_year: luke.birth_year,
+                });
+            }
+        };
+
+
+        let ignore = false;
+        fetchLuke();
+        return () => {
+            ignore = true;
+        };
+    }, []);
+
+
+    return (
+        <div>
+            Name:{" "}
+            {person
+                ? `Name: ${person.name}, Eyes: ${person.eye_color}, Birth year: ${person.birth_year}`
+                : "Loading..."}
+        </div>
+    );
+};
+
+
+export default Person;
