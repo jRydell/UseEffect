2ad5234cc2017bb69b5c8c47c2c4969bf4e7868b
Johan Rydell <johannilsrydell89@gmail.com>
2024-03-17 17:22:38 +0100

    .

--- src/Person.tsx
+++ src/Person.tsx
@@ -1,50 +1,82 @@
 import { useEffect, useState } from "react";
 
-
 type SWPerson = {
-    name: string;
-    eye_color: string;
-    birth_year: string;
+  name: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+  skin_color: string;
+  hair_color: string;
+  films: [];
+  homeworld: string;
+  terrain: string;
+  climate: string;
+  species: string;
+  language: string;
 };
-
 
 const Person = () => {
-    //const [name, setName] = useState("");
-    const [person, setPerson] = useState({} as SWPerson);
+  //const [name, setName] = useState("");
+  const [person, setPerson] = useState({} as SWPerson);
 
+  useEffect(() => {
+    const url = "https://swapi.py4e.com/api/people/1/";
 
-    useEffect(() => {
-        const url = "https://swapi.py4e.com/api/people/1/";
-        const fetchLuke = async () => {
-            const result = await fetch(url);  // result kan heta response
-            const luke = await result.json(); //luke kan heta data
-            if (!ignore) {
-                setPerson({
-                    name: luke.name,
-                    eye_color: luke.eye_color,
-                    birth_year: luke.birth_year,
-                });
-            }
-        };
+    const fetchData = async () => {
+      const response = await fetch(url);
+      const data = await response.json();
 
+      const homeworldResponse = await fetch(data.homeworld);
+      const homeworldData = await homeworldResponse.json();
 
-        let ignore = false;
-        fetchLuke();
-        return () => {
-            ignore = true;
-        };
-    }, []);
+      const speciesResponse = await fetch(data.species);
+      const speciesData = await speciesResponse.json();
 
+      if (!ignore) {
+        setPerson({
+          name: data.name,
+          eye_color: data.eye_color,
+          birth_year: data.birth_year,
+          gender: data.gender,
+          skin_color: data.skin_color,
+          hair_color: data.hair_color,
+          films: data.films.length,
+          homeworld: homeworldData.name,
+          terrain: homeworldData.terrain,
+          climate: homeworldData.climate,
+          species: speciesData.name,
+          language: speciesData.name,
+        });
+      }
+    };
 
-    return (
-        <div>
-            Name:{" "}
-            {person
-                ? `Name: ${person.name}, Eyes: ${person.eye_color}, Birth year: ${person.birth_year}`
-                : "Loading..."}
-        </div>
-    );
+    let ignore = false;
+    fetchData();
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
+  return (
+    <div>
+      {person
+        ? `Name: ${person.name}, 
+        Eyes: ${person.eye_color}, 
+        Birth year: ${person.birth_year} 
+        Gender: ${person.gender} 
+        Skin color: ${person.skin_color}
+        Hair color: ${person.hair_color}
+        Films: ${person.films}
+        Homeworld: ${person.homeworld}
+        Terrain: ${person.terrain}
+        Climate: ${person.climate}
+        Species: ${person.species}
+        Species: ${person.language}
+        
+        `
+        : "Loading..."}
+    </div>
+  );
 };
-
 
 export default Person;
